97047f0d01bd5a21fc5d8abea71361da713522dd
Özcan Elmacı <oz.ozcan.elmaci@55gmail.com>
2023-03-30 14:05:49 +0300

    30 Mart 2023

--- src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
@@ -8,6 +8,7 @@ import kodlama.io.rentacar.business.dto.responses.get.GetBrandResponse;
 import kodlama.io.rentacar.entities.Brand;
 import kodlama.io.rentacar.repository.BrandRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -17,16 +18,15 @@ import java.util.List;
 @AllArgsConstructor
 public class BrandManager implements BrandService {
     private final BrandRepository repository;
+    private final ModelMapper mapper;
 
     @Override
     public List<GetAllBrandsResponse> getAll() {
         List<Brand> brands = repository.findAll();
-        List<GetAllBrandsResponse> response = new ArrayList<>();
-
-        for (Brand brand : brands) {
-            response.add(new GetAllBrandsResponse(brand.getId(), brand.getName()));
-        }
-
+        List<GetAllBrandsResponse> response = brands
+                .stream()
+                .map(brand -> mapper.map(brand, GetAllBrandsResponse.class))
+                .toList();
         return response;
     }
 
@@ -34,22 +34,29 @@ public class BrandManager implements BrandService {
     public GetBrandResponse getById(int id) {
         checkIfBrandExists(id);
         Brand brand = repository.findById(id).orElseThrow();
-        GetBrandResponse response = new GetBrandResponse();
-        response.setId(brand.getId());
-        response.setName(brand.getName());
+       // GetBrandResponse response = new GetBrandResponse();
+        //response.setId(brand.getId());
+      //  response.setName(brand.getName());
+        GetBrandResponse response = mapper.map(brand, GetBrandResponse.class);
 
         return response;
     }
 
     @Override
     public CreateBrandResponse add(CreateBrandRequest request) {
-        Brand brand = new Brand();
+
+        Brand brand = mapper.map(request, Brand.class);
+        brand.setId(0);
+        repository.save(brand);
+
+        CreateBrandResponse response = mapper.map(brand, CreateBrandResponse.class);
+        /*Brand brand = new Brand();
         brand.setName(request.getName());
         repository.save(brand);
 
         CreateBrandResponse response = new CreateBrandResponse();
         response.setId(brand.getId());
-        response.setName(brand.getName());
+        response.setName(brand.getName());*/
 
         return response;
     }
